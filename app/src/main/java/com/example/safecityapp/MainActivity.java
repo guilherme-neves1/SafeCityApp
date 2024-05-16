2b6c69456fc6869601c0b8dae431386e2faae1dc
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-05-16 17:09:22 -0300

    Create maps and integration API on app

--- app/src/main/java/com/example/safecityapp/MainActivity.java
+++ app/src/main/java/com/example/safecityapp/MainActivity.java
@@ -1,30 +1,24 @@
 package com.example.safecityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.util.Log;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
     Button loginBtn, createAccBtn;
     EditText emailInput, passwordInput;
-
-    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-        mapFragment.getMapAsync(this);
 
         emailInput = (EditText) findViewById(R.id.emailInput);
         passwordInput = (EditText) findViewById(R.id.passwordInput);
@@ -41,9 +35,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 Log.i("Test Credentials", "Email: " + email + " and Password: " + password);
             }
         });
+
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irParaMapa();
+            }
+        });
     }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+
+    private void irParaMapa() {
+        Intent googleMaps = new Intent(this, MapsActivity.class);
+        startActivity(googleMaps);
     }
+
 }
