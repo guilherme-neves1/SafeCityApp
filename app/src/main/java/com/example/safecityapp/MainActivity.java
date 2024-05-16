32ab8f0b127405b1462d48fdd68d212c95e7b53a
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-05-16 00:17:57 -0300

    create variables login

--- app/src/main/java/com/example/safecityapp/MainActivity.java
+++ app/src/main/java/com/example/safecityapp/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.safecityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn, createAccBtn;
+    EditText emailInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        createAccBtn = (Button) findViewById(R.id.createAccBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                Log.i("Test Credentials", "Email: " + email + " and Password: " + password);
+            }
+        });
+
     }
 }
