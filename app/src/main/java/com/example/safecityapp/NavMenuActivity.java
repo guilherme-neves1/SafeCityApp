a60240c31d43490a63335830714dfe9799f582f6
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-06-01 10:12:09 -0300

    build menu 2

--- app/src/main/java/com/example/safecityapp/NavMenuActivity.java
+++ app/src/main/java/com/example/safecityapp/NavMenuActivity.java
@@ -1,94 +1,70 @@
 package com.example.safecityapp;
 
-import androidx.activity.OnBackPressedCallback;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.MenuItem;
 
-public class NavMenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class NavMenuActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nav_menu);
+        setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_post:
+                        fragment = new PostFragment();
+                        break;
+                    case R.id.nav_map:
+                        // Inicia a MapsActivity
+                        Intent intent = new Intent(NavMenuActivity.this, MapsActivity.class);
+                        startActivity(intent);
+                        drawerLayout.closeDrawers();
+                        return true;  // Retorna true aqui para evitar substituição do fragment
+                }
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+                if (fragment != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_container, fragment);
+                    transaction.commit();
+                }
 
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
 
+        // Load the default fragment
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
             navigationView.setCheckedItem(R.id.nav_home);
-        }
-
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_post:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PostFragment()).commit();
-                break;
-
-            case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                break;
-
-            case R.id.nav_map:
-                startActivity(new Intent(this, MapsActivity.class));
-                break;
-
-            case R.id.nav_police:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PoliceFragment()).commit();
-                break;
-
-            case R.id.nav_settings:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
-                break;
-
-            case R.id.nav_help:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HelpFragment()).commit();
-                break;
-
-            case R.id.nav_about:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
-                break;
-
-            case R.id.nav_logout:
-                Toast.makeText(this, "Saindo da conta", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            NavMenuActivity.super.onBackPressed();
         }
     }
 }
+
